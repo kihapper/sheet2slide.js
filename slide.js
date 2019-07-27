648eff0e73aac5650766e202df00025a0f11e60a
kihapper <t12314tk@gmail.com>
2019-07-27 14:16:45 +0200

    Changed the quote to chuck norris

--- slide.js
+++ slide.js
@@ -2,21 +2,27 @@
 //Getting the tag!
 
 
-let pageNumber = 0;
+let pageNumber = 1;
 
 
 //——————Fetching JSON data ———————
 
 let dataset = [];
+let color_random = 100;
 
-fetch("textdata.json").then(function(response){
-    return response.json();
-}).then (function(jsonData){
-    console.log(jsonData);
-    dataset = jsonData;
-    updateSection();
+//http://api.icndb.com/jokes
+//https://api.superhi.com/api/test/jokes/
+
+fetch("http://api.icndb.com/jokes")
+    .then(response => response.json())
+    .then(jsonData => {
+        console.log(jsonData);
+        dataset = jsonData;
+        updateSection();
 })
 
+
+//——————
 
 
 const importTag = document.querySelector("h1");
@@ -43,24 +49,30 @@ randomTag.addEventListener("click",function(){
 
 
 
-const tagImport =() =>{
-
-    if(dataset.length > 0){
-    }
-
-    importTag.innerHTML = "Loading script from outside" + Math.random();
-}
-
 //Make sure to update everything here.
 const updateSection= () => {
-    pagenumTag.innerHTML = pageNumber + "/" + dataset.length;
-    outputTag.innerHTML = dataset[pageNumber].copy;
-    circleTag.style.backgroundColor =  dataset[pageNumber].circle;
-    bodyTag.style.backgroundColor =  dataset[pageNumber].background;
-    tagImport();
+
+    pagenumTag.innerHTML = pageNumber + "/" + dataset.value.length;
+    outputTag.innerHTML = "&ldquo;" + dataset.value[pageNumber].joke + "&rdquo;";
+    //importTag.innerHTML = dataset[pageNumber].categories;
+
+    if(dataset.value[pageNumber].joke.length > 100){
+        outputTag.classList.add("long");
+        console.log(dataset.value[pageNumber].joke.length);
+    }
+    else{
+        outputTag.classList.remove("long");
+        console.log(dataset.value[pageNumber].joke.length);
+    }
+
+    color_random = Math.random()*360;
+
+    circleTag.style.backgroundColor =  `hsl(${color_random},100%,50%)`;
+    bodyTag.style.backgroundColor =  `hsl(${color_random},100%,70%)`;
+    outputTag.style.color = `hsl(${color_random},50%,30%)`;
+    importTag.style.color = `hsl(${color_random},30%,30%)`;
+
 }
-
-
 
 
 //——————— Change Script —————————//
@@ -86,7 +98,7 @@ const previous = () => {
 }
 
 const randomChange = () => {
-    pageNumber = Math.floor(Math.random() * dataset.length);
+    pageNumber = Math.floor(Math.random() * dataset.value.length);
     updateSection();
 }
 
