0c7bcb3d6e805c6765ff01618f39a10aa9ba46a6
kihapper <t12314tk@gmail.com>
2019-07-31 13:52:47 +0200

    Added a function to connect to google docs

--- slide.js
+++ slide.js
@@ -1,32 +1,9 @@
 
-//Getting the tag!
 
+// Getting HTML elements————————
 
-let pageNumber = 1;
-
-
-//——————Fetching JSON data ———————
-
-let dataset = [];
-let color_random = 100;
-
-//http://api.icndb.com/jokes
-//https://api.superhi.com/api/test/jokes/
-
-fetch("http://api.icndb.com/jokes")
-    .then(response => response.json())
-    .then(jsonData => {
-        console.log(jsonData);
-        dataset = jsonData;
-        updateSection();
-})
-
-
-//——————
-
-
-const importTag = document.querySelector("h1");
-const outputTag = document.querySelector("h2");
+const subtextTag = document.querySelector("h1");
+const maintextTag = document.querySelector("h2");
 const pagenumTag = document.querySelector("div.pagenum");
 const nextTag = document.querySelector("img.next");
 const prevTag = document.querySelector("img.prev");
@@ -49,28 +26,73 @@ randomTag.addEventListener("click",function(){
 
 
 
+//——————Fetching JSON data ———————
+let dataNumber = 0;
+let dataset = [];
+let color_random = 100;
+
+
+//Google Tag JS ->  1g-zeqt53tnIEecFTg6mu5yXU42dIP9uQXUHVq5eI_h8
+//https://spreadsheets.google.com/feeds/list/1g-zeqt53tnIEecFTg6mu5yXU42dIP9uQXUHVq5eI_h8/1/public/values?alt=json
+
+
+ // Fetching Google Docs link —————————
+ // Good tutorial to follow : https://medium.com/dali-lab/google-sheets-and-json-easy-backend-e29e9ef3df2
+
+const url = "https://spreadsheets.google.com/feeds/list/1g-zeqt53tnIEecFTg6mu5yXU42dIP9uQXUHVq5eI_h8/1/public/values?alt=json"
+let googleSheet = [];
+let numberText = []; 
+let subText = []; 
+let mainText = []; 
+
+fetch(url)
+    .then(function(response){
+            return response.json();
+    })
+    .then(function(data){
+        googleSheet = data.feed.entry;
+        console.log(googleSheet);
+        for (var i = 0; i < googleSheet.length; i += 1){
+            subText.push(googleSheet[i].gsx$subtext.$t);
+            mainText.push(googleSheet[i].gsx$maintext.$t);
+            numberText.push(googleSheet[i].gsx$number.$t);
+        }
+        console.log("Everything is now loaded...!")
+        console.log("Length of Google Docs" + googleSheet.length)
+
+        updateSection();
+    }).catch(function(){
+        console.log("Can't load Google Docs!");
+    })
+
+
+
+
 //Make sure to update everything here.
 const updateSection= () => {
 
-    pagenumTag.innerHTML = pageNumber + "/" + dataset.value.length;
-    outputTag.innerHTML = "&ldquo;" + dataset.value[pageNumber].joke + "&rdquo;";
-    //importTag.innerHTML = dataset[pageNumber].categories;
+    pagenumTag.innerHTML = dataNumber + "/" + (googleSheet.length - 1);
+    maintextTag.innerHTML = "&ldquo;" + mainText[dataNumber] + "&rdquo;";
+    subtextTag.innerHTML =  subText[dataNumber] + " - page :  " +  numberText[dataNumber];
 
-    if(dataset.value[pageNumber].joke.length > 100){
-        outputTag.classList.add("long");
-        console.log(dataset.value[pageNumber].joke.length);
+
+    if(mainText[dataNumber].length > 200){
+        console.log("The text is long so I resised it text :" + mainText[dataNumber].length);
+        maintextTag.classList.add("long");
     }
     else{
-        outputTag.classList.remove("long");
-        console.log(dataset.value[pageNumber].joke.length);
+        maintextTag.classList.remove("long");
     }
+
+
+    //---Changing the pretty colors
 
     color_random = Math.random()*360;
 
     circleTag.style.backgroundColor =  `hsl(${color_random},100%,50%)`;
     bodyTag.style.backgroundColor =  `hsl(${color_random},100%,70%)`;
-    outputTag.style.color = `hsl(${color_random},50%,30%)`;
-    importTag.style.color = `hsl(${color_random},30%,30%)`;
+    maintextTag.style.color = `hsl(${color_random},50%,30%)`;
+    subtextTag.style.color = `hsl(${color_random},30%,30%)`;
 
 }
 
@@ -79,26 +101,26 @@ const updateSection= () => {
 
 
 const next = () => {
-    pageNumber = pageNumber + 1;
+    dataNumber = dataNumber + 1;
 
-    if(pageNumber > dataset.length){
-        pageNumber = 0;
+    if(dataNumber > googleSheet.length - 1 ){
+        dataNumber = 0;
        }
 
     updateSection();
 }
 
 const previous = () => {
-    pageNumber = pageNumber - 1;
+    dataNumber = dataNumber - 1;
 
-    if(pageNumber < 1){
-        pageNumber = 0;
+    if(dataNumber < 1){
+        dataNumber = 0;
     }
     updateSection();
 }
 
 const randomChange = () => {
-    pageNumber = Math.floor(Math.random() * dataset.value.length);
+    dataNumber = Math.floor(Math.random() * googleSheet.length);
     updateSection();
 }
 
@@ -119,6 +141,7 @@ if(keyis.key == "ArrowLeft"){
 }
 
 })
+
 
 //——————— Initialise —————————//
 
