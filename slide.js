686008629a7b1e3f5efc7c06643ee0622351f1d5
kihapper <playful.intervention@gmail.com>
2019-08-08 21:11:22 +0200

    update html

--- slide.js
+++ slide.js
@@ -11,6 +11,12 @@ const randomTag = document.querySelector("img.random");
 const circleTag = document.querySelector("div.circle");
 const bodyTag = document.querySelector("body");
 
+const leftTag = document.querySelector('#left');
+const rightTag = document.querySelector('#right');
+const topTag = document.querySelector('#top');
+const bottomTag = document.querySelector('#bottom');
+
+
 
 nextTag.addEventListener("click",function(){
     next();
@@ -25,19 +31,23 @@ randomTag.addEventListener("click",function(){
 });
 
 
+/*—————————
+Fetching Google Docs link 
+Good tutorial to follow : https://medium.com/dali-lab/google-sheets-and-json-easy-backend-e29e9ef3df2
+Google Tag JS ->  1g-zeqt53tnIEecFTg6mu5yXU42dIP9uQXUHVq5eI_h8
+https://spreadsheets.google.com/feeds/list/1g-zeqt53tnIEecFTg6mu5yXU42dIP9uQXUHVq5eI_h8/1/public/values?alt=json
+————————— */
 
-//——————Fetching JSON data ———————
+//——————Initial Data Array ———————
+
 let dataNumber = 0;
 let dataset = [];
 let color_random = 100;
+let border_color;
+let text_color;
 
 
-//Google Tag JS ->  1g-zeqt53tnIEecFTg6mu5yXU42dIP9uQXUHVq5eI_h8
-//https://spreadsheets.google.com/feeds/list/1g-zeqt53tnIEecFTg6mu5yXU42dIP9uQXUHVq5eI_h8/1/public/values?alt=json
 
-
- // Fetching Google Docs link —————————
- // Good tutorial to follow : https://medium.com/dali-lab/google-sheets-and-json-easy-backend-e29e9ef3df2
 
 const url = "https://spreadsheets.google.com/feeds/list/1g-zeqt53tnIEecFTg6mu5yXU42dIP9uQXUHVq5eI_h8/1/public/values?alt=json"
 let googleSheet = [];
@@ -76,24 +86,44 @@ const updateSection= () => {
     subtextTag.innerHTML =  subText[dataNumber];
 
 
+    //Change the size of the text depending on the length of the text
     if(mainText[dataNumber].length > 200){
-        console.log("The text is long so I resised it text :" + mainText[dataNumber].length);
+        console.log("The text is long so I changed the text sise :" + mainText[dataNumber].length);
         maintextTag.classList.add("long");
+        maintextTag.classList.remove("short");
+
+    }
+    else if(mainText[dataNumber].length < 30){
+        console.log("The text is short so I changed the text sise :" + mainText[dataNumber].length);
+        maintextTag.classList.add("short");
+        maintextTag.classList.remove("long");
+
     }
     else{
         maintextTag.classList.remove("long");
+        maintextTag.classList.remove("short");
     }
 
 
-    //---Changing the pretty colors
+    //---Changing it into pretty colors
 
     color_random = Math.random()*360;
 
-    circleTag.style.backgroundColor =  `hsl(${color_random},100%,50%)`;
+    //Deciding the color of the border and circle.
+
     bodyTag.style.backgroundColor =  `hsl(${color_random},100%,70%)`;
-    maintextTag.style.color = `hsl(${color_random},50%,30%)`;
-    subtextTag.style.color = `hsl(${color_random},50%,30%)`;
-    pagenumTag.style.color = `hsl(${color_random},50%,30%)`;
+    circleTag.style.backgroundColor =  `hsl(${color_random},100%,50%)`;;
+
+    text_color = `hsl(${color_random},50%,30%)`;
+    maintextTag.style.color = text_color;
+    subtextTag.style.color = text_color;
+    pagenumTag.style.color = text_color;
+
+    border_color = `hsl(${color_random},50%,60%)`;
+    leftTag.style.backgroundColor = border_color;
+    rightTag.style.backgroundColor = border_color;
+    topTag.style.backgroundColor = border_color;
+    bottomTag.style.backgroundColor = border_color;
 
 }
 
@@ -143,6 +173,4 @@ if(keyis.key == "ArrowLeft"){
 
 })
 
-
-//——————— Initialise —————————//
 
